20b2b0baccfd7ef9fb5c6e9fa380a4ba2660d006
YojiTsukada <>
2018-05-01 23:01:36 +0900

    validation pipeline の設定追加

--- myproject/myproject/pipelines.py
+++ myproject/myproject/pipelines.py
@@ -9,3 +9,20 @@
 class MyprojectPipeline(object):
     def process_item(self, item, spider):
         return item
+
+
+from scrapy.exceptions import DropItem
+
+class ValidationPipeline(object):
+    """
+    Itemを検証するPipeline
+    """
+
+    def process_item(self, item, spider):
+        if not item['title']:
+            # titleフィールドが取得できていない場合は、破棄する。
+            # DropItem()の引数は破棄する理由を表すメッセージ
+            raise DropItem('Missing title')
+
+
+        return item # titleフィールドが正しく取得できている場合
